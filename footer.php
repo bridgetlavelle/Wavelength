317ba6323423405af2e22001d928330fd5c33aa1
bridgetlavelle <44756269+bridgetlavelle@users.noreply.github.com>
2018-11-11 17:22:25 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,5 @@
-
+<!-- ######################     Start of Page footer   ############################## -->
+<footer>
+	<p>Designed by Aaron Delin and Bridget Lavelle</p>
+</footer>
+<!-- ######################     End of Page footer   ############################## -->
